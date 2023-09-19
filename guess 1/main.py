d06dc7be3c941904c8c22564ceb6e3784cceef78
shahid <shahidalloor27@gmail.com>
2023-09-19 18:01:36 +0530

    123

--- guess 1/main.py
+++ guess 1/main.py
@@ -1,1 +1,19 @@
-print("hi")
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.radint(low,high)
+        else:
+            guess = low
+
+        feedback = input(f'is {guess} too high(h), too low(l),or correct (c)??: ')
+
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'yay! the computer guessed your number{guess} correctly')  
+    computer_guess(10)    
